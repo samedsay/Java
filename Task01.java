a85043266120f8bf1684bcb8c45c12450f89fde1
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2018-12-27 21:10:23 +0100

    Update Task01.java

--- Task01.java
+++ Task01.java
@@ -22,7 +22,9 @@ public class Task01{
         int num2 = scan.nextInt();
         int num3 = scan.nextInt();
 
-        if (num3 != num1 && num3 != num2)
+        if (num1 == num2 && num1 == num3)
+            System.out.println("Not Working!");
+        else if(num1 != num2 && num1 != num3 && num2 != num3)
             System.out.println("Not Working!");
         else
             System.out.println("Working!");
