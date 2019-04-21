2f5ad47e70a7ced1a7e701d66fe356f2b31af544
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2019-04-21 17:26:11 +0200

    Create Task10.java

---
+++ Classes/Task10.java
@@ -0,0 +1,108 @@
+import java.util.*;
+
+class Person implements Comparable<Person> {
+    final static int currentYear =
+            java.util.Calendar.getInstance().get(Calendar.YEAR);
+    String name;
+    int birtYear;
+    int height;
+
+    Person(String n, int y, int h) {
+        name = n;
+        birtYear = y;
+        height = h;
+    }
+
+    public int compareTo(Person o) {
+        int k = name.compareToIgnoreCase(o.name);
+        if (k != 0)
+            return k;
+        k = o.birtYear - birtYear;
+        if (k != 0)
+            return k;
+        return height - o.birtYear;
+    }
+
+    public String toString() {
+        return name + "(" + (currentYear - birtYear) +
+                "/" + height + ")";
+    }
+}
+
+class Comp1 implements Comparator<Person> {
+    public int compare(Person o1, Person o2) {
+        int k = o1.height - o2.height;
+        if (k != 0)
+            return k;
+        k = o2.birtYear - o1.birtYear;
+        if (k != 0)
+            return k;
+        return o1.name.compareToIgnoreCase(o2.name);
+    }
+}
+
+class Comp2 implements Comparator<Person> {
+    public int compare(Person o1, Person o2) {
+        int k = o2.birtYear - o1.birtYear;
+        if (k != 0)
+            return k;
+        k = o1.name.compareToIgnoreCase(o2.name);
+        if (k != 0)
+            return k;
+        return o1.height - o2.height;
+    }
+}
+
+public class Task10 {
+    public static void main(String[] args) {
+        new Task10();
+    }
+
+    Task10() {
+        List<Person> list = new ArrayList<Person>();
+        list.add(new Person("K", 1980, 165));
+        list.add(new Person("B", 1986, 171));
+        list.add(new Person("K", 1980, 168));
+        list.add(new Person("H", 1980, 171));
+        list.add(new Person("M", 1980, 171));
+        list.add(new Person("K", 1980, 169));
+        list.add(new Person("B", 1979, 171));
+        list.add(new Person("G", 1975, 171));
+
+        Collections.sort(list);
+        writeL(list, "Natural: name, age, height");
+
+        Collections.sort(list, new Comp1());
+        writeL(list, "Comp1:   height, age, name");
+
+        Comparator<Person> comp2 = new Comp2();
+        Collections.sort(list, comp2);
+        writeL(list, "Comp2:   age, name, height");
+
+        Collections.sort(list, new Comparator<Person>() {
+            @Override
+            public int compare(Person p, Person q) {
+                int k = p.name.compareToIgnoreCase(q.name);
+                if (k != 0)
+                    return k;
+                k = p.height - q.height;
+                if (k != 0)
+                    return k;
+                return q.birtYear - p.birtYear;
+            }
+        });
+
+        writeL(list, "Anonym:    name, height, age");
+
+        Collections.sort(list, (f, s) -> f.height - s.height);
+        writeL(list, "Lambda:   name, height, age");
+
+    }
+
+    static void writeL(List<Person> list, String header) {
+        System.out.println('\n' + header);
+        for (Person p : list)
+            System.out.print(p + " ");
+        System.out.println();
+    }
+}
