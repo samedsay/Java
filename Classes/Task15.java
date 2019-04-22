68eff2a95b61a66aaed53a6f2ffc1dd3c56673c0
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2019-04-22 12:17:36 +0200

    Anonymous Class Example 2.

---
+++ Classes/Task15.java
@@ -0,0 +1,39 @@
+abstract class Animal {
+    String name;
+    double weight;
+
+    public Animal(String name, double weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    abstract public String speak();
+
+    @Override
+    public String toString() {
+        return name + "(" + weight + ")" + speak();
+    }
+}
+
+public class Task14 {
+    public static void main(String[] args) {
+        Animal max = new Animal("Max", 15) {
+            @Override
+            public String speak() {
+                return "bow - bow";
+            }
+        };
+
+        Animal[] animals = {
+                max,
+                new Animal("Batty", 3.5) {
+                    @Override
+                    public String speak() {
+                        return "miaou - miaou";
+                    }
+                }
+        };
+        for (Animal a : animals)
+            System.out.println(a);
+    }
+}
