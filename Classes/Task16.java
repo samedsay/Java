467be23444b1221c228294c185f27858f581a92c
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2019-04-22 15:48:28 +0200

    Lambdas are used in this code.
    (int x, int y) -> x + y
    (x, y) -> x*y < 0
    () -> Math.random()
    e -> System.out.println(e)

---
+++ Classes/Task16.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+
+class Person {
+    private String name;
+    private int year;
+
+    public Person(String name, int year) {
+        this.name = name;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String toString() {
+        return name + "(" + year + ")";
+    }
+
+    static void show(Person[] persons, String message) {
+        System.out.println(message);
+        for (Person person : persons)
+            System.out.println(person + " ");
+        System.out.println();
+    }
+}
+
+public class Task14 {
+    public static void main(String[] args) {
+        Person[] persons = {
+                new Person("Mary", 1990),
+                new Person("Joan", 1992),
+                new Person("Suzy", 1992),
+                new Person("Beth", 1992),
+                new Person("Suzy", 1980),
+                new Person("Katy", 1982),
+        };
+        Person.show(persons, "At the beginning:");
+        Arrays.sort(persons, (p1, p2) -> p1.getYear() - p2.getYear());
+        Person.show(persons, "Ordered by age");
+        Arrays.sort(persons, (p1, p2) ->
+        {
+            int d = p1.getName().compareTo(p2.getName());
+            if (d != 0) {
+                return d;
+            }
+            return p1.getYear() - p2.getYear();
+        });
+        Person.show(persons, "Ordered by name then age");
+    }
+}
