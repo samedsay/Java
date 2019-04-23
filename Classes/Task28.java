9f64781f46f3f8710c8c93368f86f31461dfdb32
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2019-04-23 02:37:01 +0200

    Create Task28.java

---
+++ Classes/Task28.java
@@ -0,0 +1,36 @@
+public class Task28 {
+
+    public enum Season {SPRING, SUMMER, FALL, WINTER}
+
+    public static void main(String[] args) {
+
+        Season[] seasons = Season.values();
+        for (Season s : seasons)
+            System.out.print(s + " ");
+        System.out.println();
+
+        System.out.println(Season.WINTER + " is the " +
+                (Season.WINTER.ordinal() + 1) + "th season");
+        Season f = Season.valueOf("FALL");
+        System.out.println("FALL is " + f + "...");
+        System.out.println("Is f equal to FALL? " +
+                (f == Season.FALL));
+        for (Season s : seasons)
+            System.out.print(german(s) + " ");
+        System.out.println();
+    }
+
+    private static String german(Season s) {
+        switch (s) {
+            case SPRING:
+                return "Fr\u00fchling";
+            case SUMMER:
+                return "Sommer";
+            case FALL:
+                return "Herbst";
+            case WINTER:
+                return "Winter";
+        }
+        return "Cannot happen";
+    }
+}
