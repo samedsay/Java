60c0208f3f8e96850c2473dc37fe0d5a6d9bd713
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2021-07-31 23:38:27 +0200

    Create BowlingProgram.java

---
+++ Classes/BowlingProgram.java
@@ -0,0 +1,33 @@
+import java.util.*; 
+import java.util.Collections;
+public class Bowling {
+    HashMap<String, Integer> players;
+    Bowling() {
+        players = new HashMap<String, Integer>();
+    }
+    public void addPlayer(String name, int p) {
+        players.put(name, p);
+    }
+    //your code goes here
+    public void getWinner(){
+
+
+    }
+    
+}
+
+public class BowlingProgram {
+    public static void main(String[ ] args) {
+        Bowling game = new Bowling();
+        Scanner sc = new Scanner(System.in);
+
+        for(int i=0;i<3;i++) {
+            String input = sc.nextLine();
+            String[] values = input.split(" ");
+            String name = values[0];
+            int points = Integer.parseInt(values[1]);
+            game.addPlayer(name, points);
+        }
+        game.getWinner();
+    }
+}
